67f6ca83f57864d73c6825c7e2ecf98d5ab6076c
PaperBackPear3 <fabioangeloni2001@gmail.com>
2023-05-28 01:10:05 +0200

    global .gitignore and compose build

--- compose-build-up.sh
+++ compose-build-up.sh
@@ -1,8 +1,12 @@
 docker-compose -f docker-compose.yml build --no-cache
 
 ##COMPOSE UP
-echo "Starting up containers"
+echo "Starting up containers..."
 
 docker-compose -f docker-compose.yml up
-echo "Shutting down containers"
+echo "Shutting down containers..."
 docker-compose down
+#removes everything
+echo "Removing unused Images..."
+
+docker image prune --force --all
